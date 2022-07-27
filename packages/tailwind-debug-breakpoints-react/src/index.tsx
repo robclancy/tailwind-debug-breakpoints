c34e6a6d98bf0c03720dbd6bc8ead5ab5aef4887
robclancy <robbo.clancy@gmail.com>
2022-07-27 23:09:04 +1000

    change to inline styles to drop a dependency

--- packages/tailwind-debug-breakpoints-react/src/index.tsx
+++ packages/tailwind-debug-breakpoints-react/src/index.tsx
@@ -1,42 +1,41 @@
 import * as React from "react";
-import styled from "styled-components";
 import { normalizeScreens } from "tailwindcss/src/util/normalizeScreens";
 import buildMediaQuery from "tailwindcss/src/util/buildMediaQuery";
-import { Screen, ResolvableTo, ScreensConfig } from "tailwindcss/types/config";
+import { ResolvableTo, ScreensConfig } from "tailwindcss/types/config";
 
-const Container = styled.div`
-  box-sizing: border-box;
-  border-width: 0;
-  border-style: solid;
-  border-color: #e5e7eb;
-  position: fixed;
-  bottom: 0px;
-  left: 0px;
-  display: flex;
-  align-items: center;
-  border-top-right-radius: 4px;
-  background-color: rgb(17 24 39 / 0.9);
-  padding-left: 10px;
-  padding-right: 10px;
-  padding-top: 2px;
-  padding-bottom: 2px;
-  color: rgb(255 255 255);
-`;
+const containerStyles: React.CSSProperties = {
+  boxSizing: "border-box",
+  borderWidth: "0",
+  borderStyle: "solid",
+  borderColor: "#e5e7eb",
+  position: "fixed",
+  bottom: "0px",
+  left: "0px",
+  display: "flex",
+  alignItems: "center",
+  borderTopRightRadius: "4px",
+  backgroundColor: "rgb(17 24 39 / 0.9)",
+  paddingLeft: "10px",
+  paddingRight: "10px",
+  paddingTop: "2px",
+  paddingBottom: "2px",
+  color: "rgb(255 255 255)",
+};
 
-const Icon = styled.svg`
-  margin-left: -4px;
-  height: 16px;
-  width: auto;
-`;
+const iconStyles: React.CSSProperties = {
+  marginLeft: "-4px",
+  height: "16px",
+  width: "auto",
+};
 
-const Screen = styled.div`
-  box-sizing: border-box;
-  border-width: 0;
-  border-style: solid;
-  border-color: #e5e7eb;
-  color: rgb(55 65 81);
-  margin-left: 8px;
-`;
+const screenStyles: React.CSSProperties = {
+  boxSizing: "border-box",
+  borderWidth: "0",
+  borderStyle: "solid",
+  borderColor: "#e5e7eb",
+  color: "rgb(55 65 81)",
+  marginLeft: "8px",
+};
 
 type TailwindDebugBreakpointsProps = {
   screens: ResolvableTo<ScreensConfig> | undefined;
@@ -64,33 +63,33 @@ const TailwindDebugBreakpoints = ({
 
     return `@media ${buildMediaQuery(
       screenDefinition
-    )} { [data-tailwind-debug-breakpoint=b-${
+    )} { div[data-tailwind-debug-breakpoint=b${
       screenDefinition.name
-    }] { color: white } }`;
+    }] { color: white !important } }`;
   };
 
   return (
-    <Container>
-      <Icon viewBox="0 0 50 31">
+    <div style={containerStyles}>
+      <svg style={iconStyles} viewBox="0 0 50 31">
         <path
           fillRule="evenodd"
           clipRule="evenodd"
           d="M25.517 0C18.712 0 14.46 3.382 12.758 10.146c2.552-3.382 5.529-4.65 8.931-3.805 1.941.482 3.329 1.882 4.864 3.432 2.502 2.524 5.398 5.445 11.722 5.445 6.804 0 11.057-3.382 12.758-10.145-2.551 3.382-5.528 4.65-8.93 3.804-1.942-.482-3.33-1.882-4.865-3.431C34.736 2.92 31.841 0 25.517 0zM12.758 15.218C5.954 15.218 1.701 18.6 0 25.364c2.552-3.382 5.529-4.65 8.93-3.805 1.942.482 3.33 1.882 4.865 3.432 2.502 2.524 5.397 5.445 11.722 5.445 6.804 0 11.057-3.381 12.758-10.145-2.552 3.382-5.529 4.65-8.931 3.805-1.941-.483-3.329-1.883-4.864-3.432-2.502-2.524-5.398-5.446-11.722-5.446z"
           fill="#38bdf8"
         ></path>
-      </Icon>
+      </svg>
       {Object.entries(screens).map(([name, size]) => (
-        <Screen
+        <div
+          style={screenStyles}
           key={size}
-          className={`text-gray-700 ${name}:text-white`}
           title={size}
-          data-tailwind-debug-breakpoint={`b-${name}`}
+          data-tailwind-debug-breakpoint={`b${name}`}
         >
           <style>{breakpointCss(name)}</style>
           {name}
-        </Screen>
+        </div>
       ))}
-    </Container>
+    </div>
   );
 };
 
